e25e12f4b1b0168ae3b39a39816a96578ce52207
Nicolai Anton Lynnerup <nily@teknologisk.dk>
2021-01-12 08:47:35 +0100

    Added TODO

--- src/camera.py
+++ src/camera.py
@@ -6,6 +6,17 @@
 # 
 # Original author: Nicolai Anton Lynnerup <nily@dti.dk>
 
+# TODO:
+# [ ] Read all camera parameters
+#     [X] Device Info
+#     [X] Sensor Options
+#     [ ] Calibration (both intrinsic and extrinsic)
+# [X] Store parameters in a file 
+# [ ] Connect to specific camera by S/N
+# [ ] Load parameters from file and upload to camera
+# [X] Grab frames with software trigger
+# [ ] Write recursive function that can stringify dict keys.
+
 import imageio
 import numpy as np
 import os
@@ -46,24 +57,56 @@ class IntelRealSense:
         self._parameters = self._load_parameters_from_camera()
     
     def __del__(self):
+        """ Cleanly shuts down the pipeline once the garbage collector is called
+        """
         self._pipeline.stop()
 
     def get_device(self):
+        """ Returns the current device connected. In RS2 a device e.g. a D435
+            has two sensors; (1) the depth sensor and (2) the RGB sensor.
+
+        Returns:
+            rs.device: Device object from the RS2 API
+        """
         return self._device
     
     def get_depth_sensor(self):
+        """ Returns the current device's depth sensor
+
+        Returns:
+            rs.sensor: Sensor object from RS2 API
+        """
         return self._sensor_depth
     
     def get_color_sensor(self):
+        """ Returns the current device's RGB sensor
+
+        Returns:
+            rs.sensor: Sensor object from RS2 API
+        """
         return self._sensor_color
     
     def _load_parameters_from_camera(self):
+        """ Internal class method to query the camera for its info, parameters,
+            calibration, etc.
+
+        Returns:
+            dict: All info, parameters, calibration, etc.
+        """
         parameters = dict()
         parameters['info'] = self.get_info()
         parameters['options'] = self.get_options()
+        #parameters['calibration'] = {'intrinsic' : self.}
         return parameters
 
     def save_all_parameters(self, path = None):
+        """ Saves all camera parameters to a file optionally specified by
+            a path to a folder. The filename is the device name and its S/N
+
+
+        Args:
+            path (str, optional): Path to storage folder. Defaults to None.
+        """
         filename = '{}_{}.json'.format(
             self._device.get_info(rs.camera_info.name),
             self._device.get_info(rs.camera_info.serial_number)).lower().replace(' ', '_')
@@ -75,6 +118,12 @@ class IntelRealSense:
             json.dump(self._parameters, f, sort_keys=False, indent=4)
     
     def get_info(self):
+        """ Retrieve camera specific information,
+            like versions of various internal components
+
+        Returns:
+            dict: all rs.camera_info.X
+        """
         info = dict()
         info['name'] = self._device.get_info(rs.camera_info.name) 
         info['serial_number'] = self._device.get_info(rs.camera_info.serial_number)
@@ -91,6 +140,13 @@ class IntelRealSense:
         return info
 
     def get_options(self):
+        """ Returns all sensor specific options for all sensors in a device,
+            such as gain, exposure, etc.
+
+        Returns:
+            dict: Dictionary with rs.option object references and their current
+                  value
+        """ 
         options = dict()
         for sensor in self._device.sensors:
             sensor_name = sensor.get_info(rs.camera_info.name).lower().replace(' ', '_')
@@ -99,6 +155,15 @@ class IntelRealSense:
         return options
 
     def get_sensor_options(self, sensor):
+        """ Returns all sensor specific options for one specific sensor within
+            the currently connected device.
+
+        Args:
+            sensor (rs.sensor): Sensor object from the RS2 API.
+
+        Returns:
+            dict: Dictionary containing all options for the specific sensor.
+        """
         rs_options = sensor.get_supported_options()
         options = dict()
 
@@ -111,6 +176,11 @@ class IntelRealSense:
         return options
     
     def get_frames(self):
+        """ Grab a single image and return it.
+
+        Returns:
+            rs.frame: Resulting image grabbed by the device.
+        """
         frames = self._pipeline.wait_for_frames()
         return frames
 
@@ -124,6 +194,7 @@ if __name__ == "__main__":
     camera.save_all_parameters('../data')
 
     frames = camera.get_frames()
+    print(frames.get_profile().as_video_stream_profile().get_intrinsics())
     rgb = frames.get_color_frame()
     
     # Write to .png
